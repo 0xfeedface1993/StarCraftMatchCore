0ca0cb55b64fa0a57baa0161ff0db3bce3a1872d
许庭淞 <virus1993@icloud.com>
2019-04-09 16:06:18 +0800

    remove swiftLanguageVersions

--- Package.swift
+++ Package.swift
@@ -3,20 +3,6 @@
 // Date: 2019-04-09 07:01:33 +0000
 import PackageDescription
 
-/// Represents the version of the Swift language that should be used for
-/// compiling Swift sources in the package.
-public enum SwiftVersion {
-    case v3
-    case v4
-    case v4_2
-    case v5
-    
-    /// User-defined value of Swift version.
-    ///
-    /// The value is passed as-is to Swift compiler's `-swift-version` flag.
-    case version(String)
-}
-
 let package = Package(
 	name: "StarCraftMatchCore",
 	products: [
@@ -31,6 +17,5 @@ let package = Package(
 	targets: [
 		.target(name: "StarCraftMatchCore", dependencies: ["PerfectHTTPServer", "PerfectNotifications", "SQLiteStORM", "PerfectLogger"]),
 		.testTarget(name: "StarCraftMatchCoreTests", dependencies: ["StarCraftMatchCore"])
-	],
-    swiftLanguageVersions: [.v5]
+	]
 )
